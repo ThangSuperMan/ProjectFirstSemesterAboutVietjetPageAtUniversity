631e8f50afdb0189498ae566439b81aca61ee976
Phan Thắng <phanthang@Phans-MacBook-Air.local>
2022-02-05 12:45:55 +0700

    fix the banner sldier

--- JS/SliderBanner.js
+++ JS/SliderBanner.js
@@ -1,21 +1,26 @@
-const sliderImage = document.querySelector('.slider-item-image');
-console.log("super slider")
+const sliderImage = document.querySelector(".slider-item-image");
+console.log("super slider");
 
 // Variables
 let index = 0;
 
-const images = ['/images/slider1.jpeg', '/images/slider2.jpeg', '/images/slider3.jpeg', '/images/slider4.jpeg', '/images/slider5.jpeg']
+const images = [
+    "/images/slider1.jpeg",
+    "../images/slider2.jpeg",
+    "../images/slider3.jpeg",
+    "../images/slider4.jpeg",
+    "../images/slider5.jpeg",
+];
 
 // Functions
 function handleChangeImageSlider() {
-
     if (index == images.length) {
-        // Reset the first image 
+        // Reset the first image
         index = 0;
     }
 
     sliderImage.style.backgroundImage = `url('${images[index]}')`;
-    
+
     // Update index
     index++;
 }
